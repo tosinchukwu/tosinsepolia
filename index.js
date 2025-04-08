ff6655158c1ece5690a93854948d32306edc0f64
Tosinchukwu <tosin.mouseng20@gmail.com>
2025-04-08 23:06:51 +0100

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function to multiply numbers
+function times(num1, num2) {
+    return num1 * num2;
+}
+
+// Use this way
+const number1 = 8;
+const number2 = 6;
+
+// Call the function and store the result in a variable
+const result = times(number1, number2);
+
+// Output the result to the console
+console.log(result);
